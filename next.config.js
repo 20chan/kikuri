44a6100d008b2a1f8fffd1a2ec847d110d4587f6
20chan <2@0chan.dev>
2024-01-01 05:14:29 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const {
+  PHASE_PRODUCTION_BUILD,
+  PHASE_PRODUCTION_SERVER,
+} = require('next/dist/shared/lib/constants')
+
+const nextConfig = (phase) => {
+  /** @type {import('next').NextConfig} */
+
+  const isProd = (
+    process.env.NODE_ENV === 'production'
+    || phase === PHASE_PRODUCTION_BUILD
+    || phase === PHASE_PRODUCTION_SERVER
+  );
+
+  return {
+    distDir: isProd ? '.next.prod' : '.next.dev',
+  }
+}
 
 module.exports = nextConfig
