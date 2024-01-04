4ee094f2df4abdc7d0c26982e138855f51090e28
20chan <2@0chan.dev>
2024-01-04 17:37:41 +0900

    fix members....type

--- src/app/game/rps/[roomCode]/page.tsx
+++ src/app/game/rps/[roomCode]/page.tsx
@@ -3,29 +3,37 @@
 import { SocketProvider, useSocket } from '@/components/providers/SocketProvider';
 import { RPSState } from '@/lib/games/rps/rpsState';
 import type { User } from '@/lib/room/roomMember';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 import { NextProps } from 'nextjs-parser';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import Image from 'next/image';
+import { useMutex } from 'react-context-mutex';
 
 export default function RPSGamePage(props: NextProps) {
   const { params } = props;
   const roomCode = params.roomCode as string;
 
+  const { data: session } = useSession();
   const { socket } = useSocket();
   const [joined, setJoined] = useState(false);
   const [members, setMembers] = useState<User[]>([]);
   const [game, setGame] = useState(RPSState.create());
 
+  const mutexRunner = useMutex();
+
   const isHost = useMemo(() => {
-    return members[0]?.id === socket?.id;
+    return members[0]?.id === (session?.user as any)?.id;
   }, [members, socket]);
 
-  const addMember = (user: User) => {
-    if (members.find(x => x.id === user.id) === undefined) {
-      setMembers(members => [...members, user]);
-    }
+  const addMember = (...users: User[]) => {
+    setMembers(members => {
+      const usersAdd = users.filter(user => members.find(x => x.id === user.id) === undefined);
+      console.log({ members, users, usersAdd })
+      return [...members, ...usersAdd]
+    });
   };
+
+  console.log(members)
 
   useEffect(() => {
     if (!socket) return;
@@ -40,8 +48,8 @@ export default function RPSGamePage(props: NextProps) {
     }).then(async () => {
       const res = await fetch(`/api/socket/rps/${roomCode}`);
       const json = await res.json();
-      const members = json.members;
-      addMember(members[0]);
+      const members = json.members as User[];
+      addMember(...members);
       setJoined(true);
     });
 
@@ -58,25 +66,35 @@ export default function RPSGamePage(props: NextProps) {
   }, [socket]);
 
   return (
-    <div>
-      <h1>Rock Paper Scissors</h1>
-      <p>Room Code: {roomCode}</p>
+    <div className='p-24 flex flex-col items-center h-screen'>
+      <p className='text-5xl'>Rock Paper Scissors</p>
+      <p className='text-xl text-half-white/70 mt-1'>
+        Room Code: <span className='font-bold text-half-white'>{roomCode}</span>
+      </p>
 
-      {joined && members.map(member => (
-        <div key={member.id}>
-          <p>{member.name}</p>
-          <Image src={member.image} alt='user image' width={24} height={24} />
-        </div>
-      ))}
-
-      {
-        isHost && (
-          <button
-          >
-            START
-          </button>
-        )
+      {joined && <div className='flex flex-row gap-4 mt-10'>
+        {
+          members.map(member => (
+            <div key={member.id}>
+              <Image src={member.image} alt='user image' width={100} height={100} />
+              <p className='text-center font-bold text-lg'>{member.name}</p>
+            </div>
+          ))
+        }
+      </div>
       }
+
+      <div className='w-full h-full'>
+        {
+          isHost && (
+            <div className='flex flex-col justify-center items-center h-full'>
+              <button className='text-5xl'>
+                START
+              </button>
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
